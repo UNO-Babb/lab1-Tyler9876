ac513b125cf1c2b370877366428479c14390ee8d
Tyler9876 <tylermenke@unomaha.edu>
2024-08-28 16:40:36 +0000

    first update

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,20 +1,23 @@
 #FirstProgram.py
-#Name:
-#Date:
-#Assignment:
+#Name: Tyler Menke
+#Date: 08/28/24
+#Assignment: Lab 1
 
 def main():
   print("First Program")
   #Say hello
-  
+  print("hello, what a beautiful day it is today")
   #Ask for the user's name
-
+  name = input("What is your name? ")
   #Use the user's name in the program.
-
+  print("nice to see you", name)
   #Ask the user for their age.
-
+  age = input ("How old are you?")
   #Tell the user what year they were born in.
+  age = int(age)
+  birthYear = 2024 - age
   #Assume that they have not had their birthday yet this year.
+  print("you were born in: ", birthYear)
 
 
 #Call the main function if this is the file being run.
--- MadLib.py
+++ MadLib.py
@@ -1,13 +1,20 @@
 #MadLib.py
-#Name:
-#Date:
-#Assignment:
+#Name: Tyler menke
+#Date: 08/28/24
+#Assignment: Lab 1
 
 def main():
   print("Madlib")
   #Ask user for words
-
+  location1 = input("Give a location: ")
+  noun1 = input("Give an noun: ")
+  action1 = input("Give an action: ")
+  action2 = input("give an action: ")
+  person1 = input("Give a person: ")
   #Print the story with the user supplied words.
+  print("Tuesday, I took a trip to the", location1, "with some friends, when we suddenly saw a", noun1, ".")
+  print("We were not quite sure what it was, but we decided to",action1, ".", "Man am I glad we decided to", action1, "but I wonder what would have happened if we had decide to", action2, ".")
+  print("This will make for an excelent story to tell", person1, ".")
 
 
 
