2c6e894b38cdbe0b6343fd731d3ca80ae9210360
Tyler9876 <tylermenke@unomaha.edu>
2024-08-29 14:31:44 +0000

    Finished MadLib.Py

--- MadLib.py
+++ MadLib.py
@@ -11,10 +11,11 @@ def main():
   action1 = input("Give an action: ")
   action2 = input("give an action: ")
   person1 = input("Give a person: ")
+  hobby1 = input("Give a fun hobby: ")
   #Print the story with the user supplied words.
-  print("Tuesday, I took a trip to the", location1, "with some friends, when we suddenly saw a", noun1, ".")
-  print("We were not quite sure what it was, but we decided to",action1, ".", "Man am I glad we decided to", action1, "but I wonder what would have happened if we had decide to", action2, ".")
-  print("This will make for an excelent story to tell", person1, ".")
+  print("Tuesday, I took a trip to the", location1, "with some friends, when we suddenly saw a", noun1, "on the side of the road.")
+  print("We were not quite sure what it was, but we decided to",action1, " away from it.", "Man am I glad we decided to", action1, "but I wonder what would have happened if we had decide to", action2, "instead.")
+  print("I think this will make for an excelent story to tell", person1, "if I get the chance. They will definitly enjoy it. Speaking of, I should check up on them and see if they are still into", hobby1)
 
 
 
